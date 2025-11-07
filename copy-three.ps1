caed2e6c263393adb949f0299ea114e10d10e648
Ribhav-Singla <ribhavsingla2166@gmail.com>
2025-11-07 21:54:11 +0530

    Add three.core.js dependency required by three.module.js

--- copy-three.ps1
+++ copy-three.ps1
@@ -6,8 +6,9 @@ $publicDir = "d:\mujoco_wasm - Copy\public\three"
 New-Item -ItemType Directory -Force -Path "$publicDir\build" | Out-Null
 New-Item -ItemType Directory -Force -Path "$publicDir\examples\jsm" | Out-Null
 
-# Copy main Three.js file
+# Copy all necessary Three.js build files
 Copy-Item "d:\mujoco_wasm - Copy\node_modules\three\build\three.module.js" "$publicDir\build\" -Force
+Copy-Item "d:\mujoco_wasm - Copy\node_modules\three\build\three.core.js" "$publicDir\build\" -Force
 
 # Copy examples/jsm directory recursively
 Copy-Item "d:\mujoco_wasm - Copy\node_modules\three\examples\jsm\*" "$publicDir\examples\jsm\" -Recurse -Force
