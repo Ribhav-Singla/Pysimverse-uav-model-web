d5230f0001b22aca5f42e1b626931bda8b59135a
Ribhav-Singla <ribhavsingla2166@gmail.com>
2025-11-16 14:26:11 +0530

    changed time to 15 min

--- render-cron/cron.js
+++ render-cron/cron.js
@@ -1,13 +1,13 @@
 import cron from "node-cron";
 import { init } from "./producer.js";
 
-// Run every 1 hour
-cron.schedule("0 * * * *", () => {
-  console.log('⏰ Cron job triggered');
-  init();
-});
+// Run every 15 minutes
+// cron.schedule("*/15 * * * *", () => {
+//   console.log('⏰ Cron job triggered');
+//   init();
+// });
 
-console.log('✅ Cron job scheduled to run every 1 hour');
+console.log('✅ Cron job scheduled to run every 15 minutes');
 
 // Run immediately on startup
 console.log('🚀 Running initial job...');
